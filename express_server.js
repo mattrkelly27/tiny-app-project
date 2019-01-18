3529e0613826f56a2a13754f01858bb5402763c6
Matt Kelly <matt.r.kelly27@gmail.com>
2019-01-18 02:04:59 +0000

    login page updated

--- express_server.js
+++ express_server.js
@@ -4,17 +4,18 @@ var PORT = 8080; // default port 8080
 
 
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
-
-
-
-app.set("view engine", "ejs");
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 
 
@@ -25,9 +26,26 @@ var urlDatabase = {  //urlDatabase: keys are short url, values are long url
 
 
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+
+
+
+app.set("view engine", "ejs");
+
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 
 
@@ -37,10 +55,14 @@ app.get("/urls.json", (req, res) => {
 
 
 
+
 app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
+  let templateVars = {
+    greeting: 'Hello World!'
+  };
   res.render("hello_world", templateVars);
 });
+
 
 
 
@@ -49,88 +71,189 @@ app.get("/urls", (req, res) => {
     username: req.cookies.username,
     urls: urlDatabase
   };
-
-
-
   res.render("urls_index", templateVars);
 });
+
 
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies.user_ID
+  };
+  res.render("urls_new", templateVars);
 });
+
 
 
 
 app.post('/urls/login', (req, res) => {
-
-  res.render("urls_index", templateVars);
-
   res.cookie("username", req.body.username);
   res.redirect('/urls');
-
 });
+
 
 
 
 app.post('/urls/logout', (req, res) => {
-
   res.clearCookie("username", req.body.username);
   res.redirect('/urls');
-
 });
+
 
 
 
 app.post("/urls", (req, res) => {
   console.log("this is whole req.body ", req.body);
-  // console.log(req.body['longURL']);
   urlDatabase[generateRandomString()] = req.body['longURL'];
-  // console.log(urlDatabase);
-  res.send("You got it baby!");
+  res.redirect('/urls');
 });
 
 
 
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: users[req.params.user_ID]
+  };
+
+  console.log(req.params.user_ID);
+
   res.render("urls_show", templateVars);
 });
 
 
 
+
 app.get("/u/:shortURL", (req, res) => {
-
   let longURL = urlDatabase[req.params.shortURL];
-
   res.redirect(longURL);
 });
 
 
 
-app.post('/urls/:id/delete', (req, res) => {
 
+app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 
 
+
 app.post('urls/:id', (req, res) => {
-
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
   res.render("urls_show", templateVars);
-
   urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 
 
+
+app.get("/register", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: users[req.body.user_ID],   ////////
+    email: req.body.email,
+    password: req.body.email
+  };
+  res.render("urls_register", templateVars);
+});
+
+
+
+
+
+app.post('/register', (req, res) => {
+
+  if (req.body.email === "" || req.body.password === "" || isEmailTaken(req.body.email)) {
+    res.redirect('/register');
+    return console.log("400 bad request");
+  }
+
+  let user_id = generateRandomString();
+  users[user_id] = { id: user_id, email: req.body.email, password: req.body.password }
+
+  res.cookie("email", req.body.email);
+  res.cookie("password", req.body.password);
+  res.cookie("user_id", user_id);
+
+  res.redirect('/urls');
+});
+
+
+
+
+app.get("/login", (req, res) => {
+
+   let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: users[req.body.user_ID],   ////////
+    email: req.body.email,
+    password: req.body.email
+  };
+
+  res.render("urls_login", templateVars);
+});
+
+
+
+
+app.post('/login', (req, res) => {
+
+  let userFound = checkUserNamePassword(req.body.email, req.body.password);
+  if(userFound){
+    res.cookie('user_id', userFound.id);
+    res.redirect('/urls');
+    //redirect him to the urls
+
+  } else{
+    res.send("Sorry!. The email not found in the database");
+  }
+});
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+//1. Registration - We check whether the email is taken or not?
+//2. Login - you need to verify the email and password
+
+
+
+function checkUserNamePassword(email, password){
+  for(const userId in users){
+    const user = users[userId];
+    if(user.email === email && user.password === password){
+      return users[userId];
+    }
+  }
+}
+
+function isEmailTaken(email){
+  for(const userId in users){
+    const user = users[userId];
+    if(user.email === email){
+      return true;
+    }
+  }
+  return false;
+}
 
 
 
